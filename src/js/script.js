bd394cb385eadbf0f7c707a367a77160cff6818d
Annushka1 <robin1bobin@gmail.com>
2016-12-20 23:26:17 +0300

    добавиоа галерею на третьей странице, но без увеличения

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,4 @@
 $(document).ready(function(){
-//   $('#gallery-thumbs').owlCarousel({
-//     items: 3,
-//     loop: true,
-//     center: true,
-//     responsive: {
-//       680: {
-//         items:4,
-//       },
-//       800: {
-//         items:5,
-//       },
-//     }
-//   });
-
-//   var mainImageUrl = $('#gallery-start-photo').attr('href');
-//   $('#gallery-main-pict').append('<img src="'+mainImageUrl+'" alt="">');
-
-//   $('#gallery-thumbs a').on('click', function(e){
-//     e.preventDefault();
-//     var imgUrl = $(this).attr('href');
-//     $('#gallery-main-pict').html('<img src="'+imgUrl+'" alt="">');
-//   })
-
-
-
-//Табы на главной странице
-
-var tabs = document.querySelectorAll('.rest-type-switcher__tab .rest-type-switcher__item a');
-var typeItem = document.querySelectorAll('.rest-type-switcher__type-item');
-
-function changeTab(e) {
-  e.preventDefault()
-  for(var i=0; i<tabs.length; i++) {
-    tabs[i].parentNode.classList.remove('rest-type-switcher__item--active');
-    typeItem[i].classList.remove('rest-type-switcher__type-item--active');
-  }
-  e.target.parentNode.classList.add('rest-type-switcher__item--active');
-
-  var link = e.target.getAttribute('href');
-  document.querySelector(link).classList.add('rest-type-switcher__type-item--active');
-
-}
-
-for (var i=0; i<tabs.length; i++) {
-  tabs[i].addEventListener('click', changeTab);
-}
-
-
 
 //Выпадающее меню
 $('.gamburger').on('click', function(){
@@ -97,7 +49,7 @@ responsive: [
 
 //Карусель акций
 $('.one-time').slick({
-  dots: true,
+  dots: false,
   infinite: true,
   speed: 300,
   slidesToShow: 1,
@@ -105,9 +57,29 @@ $('.one-time').slick({
 });
 
 
+//Карусель на третьей странице
 
+//Табы на главной странице
 
+var tabs = document.querySelectorAll('.rest-type-switcher__tab .rest-type-switcher__item a');
+var typeItem = document.querySelectorAll('.rest-type-switcher__type-item');
 
+function changeTab(e) {
+  e.preventDefault()
+  for(var i=0; i<tabs.length; i++) {
+    tabs[i].parentNode.classList.remove('rest-type-switcher__item--active');
+    typeItem[i].classList.remove('rest-type-switcher__type-item--active');
+  }
+  e.target.parentNode.classList.add('rest-type-switcher__item--active');
+
+  var link = e.target.getAttribute('href');
+  document.querySelector(link).classList.add('rest-type-switcher__type-item--active');
+
+}
+
+for (var i=0; i<tabs.length; i++) {
+  tabs[i].addEventListener('click', changeTab);
+}
 
 
 // var buttonTitle = document.querySelectorAll('.type-digs__title');
@@ -133,3 +105,4 @@ $('.one-time').slick({
 
 
 });
+
