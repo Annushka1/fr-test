68218384e215ccb64cd62dcb274e316caa9a20c1
Annushka1 <robin1bobin@gmail.com>
2016-12-23 19:21:02 +0300

    перенесла скрипт их html в js

--- src/js/script.js
+++ src/js/script.js
@@ -86,43 +86,6 @@ $('.one-time-second').slick({
   arrows: true
 });
 
-// Календарь
-$( function() {
-    var from = $( "#start" ).datepicker()
-        .on( "change", function() {
-          to.datepicker( "option", "minDate", getDate( this ) );
-          var date = $(this).datepicker('getDate');
-          $( "#start-d" ).text( date.getDate() );
-          $( "#start-m" ).text( date.getMonth() + 1 );
-          $( "#start-y" ).text( date.getFullYear() );
-        }),
-      to = $( "#fin" ).datepicker()
-        .on( "change", function() {
-          from.datepicker( "option", "maxDate", getDate( this ) );
-          var date = $(this).datepicker('getDate');
-          $( "#fin-d" ).text( date.getDate() );
-          $( "#fin-m" ).text( date.getMonth() + 1 );
-          $( "#fin-y" ).text( date.getFullYear() );
-        });
- 
-    function getDate( element ) {
-      var date;
-      try {
-        date = $.datepicker.parseDate( "mm/dd/yy", element.value );
-      } catch( error ) {
-        date = null;
-      }
- 
-      return date;
-    }
-  } );
-
- //
- // $(".fancybox-thumb").fancybox();
-
-
-
-
 //Табы на главной странице
 
 var tabs = document.querySelectorAll('.rest-type-switcher__tab .rest-type-switcher__item a');
