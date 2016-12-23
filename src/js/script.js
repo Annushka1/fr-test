2f97543e049ef375e27dbd13ac63801fb34a5608
Annushka1 <robin1bobin@gmail.com>
2016-12-23 14:53:45 +0300

    сделала альтернативу fancybox

--- src/js/script.js
+++ src/js/script.js
@@ -80,19 +80,7 @@ $('.one-time-second').slick({
 // });
 
  //
-  $(".fancybox-thumb").fancybox({
-    prevEffect  : 'none',
-    nextEffect  : 'none',
-    helpers : {
-      title : {
-        type: 'outside'
-      },
-      thumbs  : {
-        width : 50,
-        height  : 50
-      }
-    }
-  });
+ // $(".fancybox-thumb").fancybox();
 
 
 
