20c21b2be7529517d78408561bf1af89277f018a
Annushka1 <robin1bobin@gmail.com>
2018-05-07 22:23:16 +0300

    карты поправила

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,41 @@
 $(document).ready(function(){
+	ymaps.ready(init); 
+    function init () {            
+        var map1 = new ymaps.Map('map1', {
+            center: [55.818392, 37.447917], //1 карта
+            zoom: 15
+        });
+
+        var map2 = new ymaps.Map('map2', {
+            center: [55.682869, 37.888900], //2 карта
+            zoom: 15
+        });
+
+        var map3 = new ymaps.Map('map3', {
+            center: [44.757904, 37.741282], //3 карта
+            zoom: 15
+        });
+
+
+ 		var myPlacemark1 = new ymaps.Placemark(
+			[55.818392, 37.447917], 
+			{iconContent: '<div class="map1">улица Водников, 2с2</div>',},
+			{preset: 'twirl#redIcon'});
+ 		var myPlacemark2 = new ymaps.Placemark(
+			[55.682869, 37.888900], 
+			{iconContent: '<div class="map2">Красная улица, 1 литД</div>',},
+			{preset: 'twirl#redIcon'});
+ 		var myPlacemark3 = new ymaps.Placemark(
+			[44.757904, 37.741282], 
+			{iconContent: '<div class="map2">Новороссийск г., с. Кирилловка, ул. 2-я ж/д петля</div>',},
+			{preset: 'twirl#redIcon'});
+
+		map1.geoObjects.add(myPlacemark1);
+		map2.geoObjects.add(myPlacemark2);
+		map3.geoObjects.add(myPlacemark3);
+        };
+
+
 
 // $('.payment__postpayment').click(function(){
 //   $('.payment__title').removeClass('check--active');
@@ -20,7 +57,6 @@ $('.gamburger').on('click', function(){
   $('.main-menu').toggleClass('fixmenu');
   // $('.main-menu').removeClass('fixmenu');
 });
-
 
 
 // $('.gamburger').on('click', function(){
@@ -123,42 +159,8 @@ style.hasHint = false;
 //   arrows: true
 // });
 
-//Табы на главной странице
+// карты
 
-// var tabs = document.querySelectorAll('.rest-type-switcher__tab .rest-type-switcher__item a');
-// var typeItem = document.querySelectorAll('.rest-type-switcher__type-item');
 
-// function changeTab(e) {
-//   e.preventDefault()
-//   for(var i=0; i<tabs.length; i++) {
-//     tabs[i].parentNode.classList.remove('rest-type-switcher__item--active');
-//     typeItem[i].classList.remove('rest-type-switcher__type-item--active');
-//   }
-//   e.target.parentNode.classList.add('rest-type-switcher__item--active');
-
-//   var link = e.target.getAttribute('href');
-//   document.querySelector(link).classList.add('rest-type-switcher__type-item--active');
-
-// }
-
-// for (var i=0; i<tabs.length; i++) {
-//   tabs[i].addEventListener('click', changeTab);
-// };
- 
-
-//  $(document).mouseup(function (e) {
-//     var container = $(".popup-content");
-//     if (container.has(e.target).length === 0){
-//         container.hide();
-//     }
-// });
-
-// $(document).mouseup(function (e){ // событие клика по веб-документу
-//     var div = $(".popup-content"); // тут указываем ID элемента
-//     if (!div.is(e.target) // если клик был не по нашему блоку
-//         && div.has(e.target).length === 0) { // и не по его дочерним элементам
-//       div.hide(); // скрываем его
-//     }
-//   });
 });
 
